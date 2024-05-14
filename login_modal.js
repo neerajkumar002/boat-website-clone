e65e3a52496cd9d7c840328e1dec9d2440839b29
Neeraj kumar <neerajkumar08697@gmail.com>
2024-05-14 18:04:13 +0530

    feat: login modal wrapper

--- login_modal.js
+++ login_modal.js
@@ -1,7 +1,9 @@
 const loginBtn = document.querySelector(".login-btn");
 const loginModal = document.querySelector(".login-modal");
 const closeLoginModal = document.querySelector(".login-modal-close");
-
+const loginModalWrapper = document.querySelector(".login-modal-wrapper");
+const openLoginModal = document.querySelector(".open-login-modal");
+const closeModalWrapper = document.querySelector(".close-modal-wrapper");
 
 loginBtn.addEventListener(
   "click",
@@ -12,5 +14,18 @@ closeLoginModal.addEventListener(
   "click",
   () => (loginModal.style.visibility = "hidden")
 );
+openLoginModal.addEventListener("click", () => {
+  loginModalWrapper.style.visibility = "visible";
+});
 
+closeModalWrapper.addEventListener("click", () => {
+  loginModalWrapper.style.visibility = "hidden";
+  loginModal.style.visibility = "hidden";
+});
 
+window.addEventListener("click", (e) => {
+  if (e.target === loginModalWrapper) {
+    loginModalWrapper.style.visibility = "hidden";
+    loginModal.style.visibility = "hidden";
+  }
+});
